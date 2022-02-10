2ccb2b10669dfa7f15c7cb51958337e765e23e11
Islam Sayed <islam.sayed8@gmail.com>
2022-02-10 17:58:21 +0200

    create socket.io backend events' handlers

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const app = require('express')();
-//const server = require('http').createServer(app);
-const cors = require('cors')
-const PORT = process.env.PORT || 3333
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const PORT = process.env.PORT || 3333;
 
-const server = app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-})
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
 
-app.use(cors())
+app.use(cors());
 
+app.get("/", (req, res) => {
+	res.send("server is running");
+});
 
-app.get('/', (req, res) => {
-    res.send("server is running")
-})
+io.on("connection", (socket) => {
+	socket.emit("me", socket, id);
+	socket.on("disconnect", () => socket.broadcast.emit("callended"));
+	socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("calluser", { signalData, from, name });
+	});
+	socket.on("answercall", (data) => {
+		io.to(data.to).emit("callaccepted", data.signal);
+	});
+});
 
+server.listen(PORT, () => {
+	console.log(`server is running on port ${PORT}`);
+});
