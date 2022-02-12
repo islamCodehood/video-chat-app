69d755b1b3200aa044f0bfa6e3cf8c6729c3ac42
Islam Sayed <islam.sayed8@gmail.com>
2022-02-12 15:12:42 +0200

    add ThemeProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from '@mui/styles'
+import { unstable_createMuiStrictModeTheme } from '@mui/material/styles';
 import './styles.css'
-
+const theme = unstable_createMuiStrictModeTheme();
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
