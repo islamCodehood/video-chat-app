bc5eebb36b3e9f08a6f1c388afd71e339d76bd6d
Islam Sayed <islam.sayed8@gmail.com>
2022-02-12 02:15:38 +0200

    import Typography, AppBAR

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import './styles.css'
+import React from 'react'
+import { Typography, AppBar } from '@mui/material'
 const App = () => {
+  return (
     <div>
-
+        <AppBar position="static" color="inherit">
+            <Typography varient="h2" align="center">
+                Video Chat App
+            </Typography>
+        </AppBar>
     </div>
+  )
 }
+
+export default App
