9b266239ebf43fc69ac81899d8b0f616636bba1a
Islam Sayed <islam.sayed8@gmail.com>
2022-02-15 16:53:07 +0200

    import ContextProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ThemeProvider } from '@mui/styles'
-import { unstable_createMuiStrictModeTheme } from '@mui/material/styles';
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "@mui/styles";
+import { unstable_createMuiStrictModeTheme } from "@mui/material/styles";
+import "./styles.css";
+import { ContextProvider } from "./context";
 const theme = unstable_createMuiStrictModeTheme();
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ContextProvider>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</ContextProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
-
-
